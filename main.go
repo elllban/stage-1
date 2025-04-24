0c6f61f2e86c65098370ad5d6998ab4cc0628ed2
ivan <radadada@adad.com>
2025-04-25 00:51:51 +0700

    1

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"net/http"
+)
+
+var task string
+
+type requestBody struct {
+	Task string `json:"task"`
+}
+
+func getTask(c echo.Context) error {
+	response := "hello, " + task
+	return c.JSON(http.StatusOK, response)
+}
+
+func postTask(c echo.Context) error {
+	var req requestBody
+	if err := c.Bind(&req); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid request"})
+	}
+
+	task = req.Task
+	return c.JSON(http.StatusCreated, task)
+}
 
 func main() {
-	fmt.Println("hui")
+	e := echo.New()
+
+	e.Use(middleware.CORS())
+	e.Use(middleware.Logger())
+
+	e.GET("/task", getTask)
+	e.POST("/task", postTask)
+
+	e.Start("localhost:8080")
 }
